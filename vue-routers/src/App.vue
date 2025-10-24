d15ce2c1d7d474232e138c061f8e2b8638e8f176
admin <sun88110@naver.com>
2025-10-24 12:40:16 +0900

    test

--- vue-routers/src/App.vue
+++ vue-routers/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/members">회원페이지</RouterLink>
       </nav>
     </div>
   </header>
---
+++ vue-routers/src/views/MemberView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>멤버페이지</h3>
+</template>
